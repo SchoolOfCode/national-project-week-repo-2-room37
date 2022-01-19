43d1d6c597c30494167eed985aac28df7e91c349
Asani <asanij@gmail.com>
2022-01-19 13:43:21 +0000

    created half of the data for database

---
+++ data.js
@@ -0,0 +1,168 @@
+const data = [
+  {
+   date: "14/11/2021",
+   title:	"css_create-a-koala",	
+   repo: "https://github.com/SchoolOfCode/css_create-a-koala",	
+   status: false
+  },
+  {
+    date: "15/11/2021",
+    title: "ConsoleDOM-Workshop",
+    repo: "https://github.com/SchoolOfCode/ConsoleDOM-Workshop",
+    status:false
+  },
+  {
+    date: "17/11/2021",
+    title: "JS101-workshop",
+    repo: "https://github.com/SchoolOfCode/JS101-workshop",
+    status:false,
+  },
+  {
+    date: "18/11/2021",
+    title: "workshop_js_objects",
+    repo: "https://github.com/SchoolOfCode/workshop_js_objects",
+    status: false
+  },
+  {
+    date: "18/11/2021",
+    title: "workshop_js_arrays",
+    repo: "https://github.com/SchoolOfCode/workshop_js_arrays",
+    status: false
+  },
+  {
+    date: "19/11/2021",
+    title: "RockPaperScissors-Hackathon",
+    repo: "https://github.com/SchoolOfCode/w1d5_rock-paper-scissors-hackathon-team-38",
+    status:false
+  },
+  {
+    date: "24/11/2021",
+    title: "workshop_clock-challenge",
+    repo: "https://github.com/SchoolOfCode/workshop_clock-challenge",
+    status:false
+  },
+  {
+    date: "24/11/2021",
+    title: "TalkingToServers_Fetch_Workshop",
+    repo: "https://github.com/SchoolOfCode/TalkingToServers_Fetch_Workshop",
+    status:false
+  },
+  {
+    date: "25/11/2021",
+    title: "workshop_array-methods",
+    repo: "https://github.com/SchoolOfCode/workshop_array-methods",
+    status:false
+  },
+  {
+    date: "26/11/2021",
+    title: "hackathon_fetch",
+    repo: "https://github.com/SchoolOfCode/hackathon_fetch",
+    status:false
+  },
+  {
+    date: "28/11/2021",
+    title: "rock-paper-scissors-workshop",
+    repo: "https://github.com/SchoolOfCode/rock-paper-scissors-workshop",
+    status:false
+  },
+  {
+    date: "29/11/2021",
+    title: "recap_tasks_week-2",
+    repo: "https://github.com/SchoolOfCode/recap_tasks_week-2",
+    status:false
+  },
+  {
+    date: "30/11/2021",
+    title: "workshops_not-light",
+    repo: "https://github.com/SchoolOfCode/workshops_not-light",
+    status:false
+  },
+  {
+    date: "02/12/2021",
+    title: "workshops_clone-a-site",
+    repo: "https://github.com/School-of-Code-HQ/workshops_clone-a-site",
+    status:false
+  },
+  {
+    date: "03/12/2021",
+    title: "recap_ui-ux_week-3",
+    repo: "https://github.com/School-of-Code-HQ/recap_ui-ux_week-3",
+    status:false
+  },
+  {
+    date: "05/12/2021",
+    title: "hackathon_fetch-recipe-app",
+    repo: "https://github.com/SchoolOfCode/w3d5_hackathon_fetch-recipe-app-superduo",
+    status:false
+  },
+  {
+    date: "06/12/2021",
+    title: "workshop_node-intro",
+    repo: "https://github.com/SchoolOfCode/workshop_node-intro",
+    status:false
+  },
+  {
+    date: "08/12/2021",
+    title: "workshops_express-101-books-api",
+    repo: "https://github.com/School-of-Code-HQ/workshops_express-101-books-api",
+    status:false
+  },
+  {
+    date: "08/12/2021",
+    title: "workshop_RESTful-API-design",
+    repo: "https://github.com/SchoolOfCode/workshop_RESTful-API-design",
+    status:false
+  },
+  {
+    date: "10/12/2021",
+    title: "hackathon_build-a-backend",
+    repo: "https://github.com/SchoolOfCode/hackathon_build-a-backend",
+    status:false
+  },
+  {
+    date: "12/12/2021",
+    title: "express-middleware-workshop",
+    repo: "https://github.com/SchoolOfCode/express-middleware-workshop",
+    status:false
+  },
+
+]
+12/12/2021	recap_node-express_week-4	https://github.com/SchoolOfCode/recap_node-express_week-4	FALSE
+23
+16/12/2021	workshops_config-management-12-factor-app	https://github.com/SchoolOfCode/workshops_config-management-12-factor-app	FALSE
+24
+16/12/2021	workshops_node-pg-codealong	https://github.com/SchoolOfCode/workshops_node-pg-codealong	FALSE
+25
+17/12/2021	hackathon_energiser-score-tracker	https://github.com/SchoolOfCode/hackathon_energiser-score-tracker	FALSE
+26
+19/12/2021	recap_sql-and-node	https://github.com/SchoolOfCode/recap_sql-and-node	FALSE
+27
+23/12/2021	workshop_make-your-own-codewars	https://github.com/SchoolOfCode/workshop_make-your-own-codewars	FALSE
+28
+05/01/2022	workshop_React-useState-pt1	https://github.com/SchoolOfCode/workshop_React-useState-pt1	FALSE
+29
+05/01/2022	workshop_React-useState-pt2	https://github.com/SchoolOfCode/workshop_React-useState-pt2	FALSE
+30
+06/01/2022	JS_ImmutableUpdates_Workshop	
+https://github.com/SchoolOfCode/w7d3_immutable_updates-team-52-fatou-kattana-marian
+FALSE
+31
+06/01/2022	workshop_react_lists-and-keys	https://github.com/SchoolOfCode/workshop_react_lists-and-keys	FALSE
+32
+07/01/2022	w7d4_react-hackathon	https://classroom.github.com/a/_iACviEW	FALSE
+33
+09/01/2022	recap_react-front-end-blog	https://github.com/SchoolOfCode/recap_react-front-end-blog	FALSE
+34
+10/01/2022	workshop_first-useEffect	https://github.com/SchoolOfCode/workshop_first-useEffect	FALSE
+35
+12/01/2022	workshops_react-practice-challenge	https://github.com/SchoolOfCode/workshops_react-practice-challenge	FALSE
+36
+12/01/2022	workshops_reduce-math-class	https://github.com/SchoolOfCode/workshops_reduce-math-class	FALSE
+37
+13/01/2022	React_Router_Workshop	https://github.com/SchoolOfCode/React_Router_Workshop	FALSE
+38
+13/01/2022	workshop_React-testing-library	https://github.com/SchoolOfCode/workshop_React-testing-library	FALSE
+39
+14/01/2022	hackathon_react-app-with-api-data	https://github.com/SchoolOfCode/hackathon_react-app-with-api-data	FALSE
+40
+17/01/2022	projects_national-project-week	https://github.com/SchoolOfCode/projects_national-project-week	FALSE
---
+++ db/scripts/createTable.js
@@ -0,0 +1,11 @@
+import { response } from "express";
+import query from "../index.js";
+
+const sqlString = `CREATE TABLE IF NOT EXISTS socTasks(id SERIAL PRIMARY KEY, title TEXT, date DATE, status BOOL, repo TEXT)`;
+
+async function createTable(){
+  const response = await query(sqlString);
+ console.log('created a table', response); 
+}
+
+createTable();
