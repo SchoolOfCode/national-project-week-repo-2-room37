f3668ff18a1b7fb18b9bff238f033b4fad2017e4
Mirella <mirellacodes@gmail.com>
2022-01-20 10:36:20 +0000

    heroku backend is working

--- db/index.js
+++ db/index.js
@@ -3,13 +3,8 @@ import pg from "pg";
 // import { databaseHost, databaseName, password, username } from "../config.js";
 
 const pool = new pg.Pool({
-  connectionString:
-    "postgres://widktucdvzrhhq:0bbe45d8c6595de3e251e5df49a1117ad92231d475fda1e0947f660f44b2c6ba@ec2-54-195-246-55.eu-west-1.compute.amazonaws.com:5432/deoq6o1kkmh5rf",
-  // user: process.env.PGUSER,
-  // host: process.env.PGHOST,
-  // password: process.env.PGPASSWORD,
-  // database: process.env.PGDATABASE,
-  // port: 5432,
+  connectionString: process.env.PGSTRING,
+
   ssl: { rejectUnauthorized: false },
 });
 
