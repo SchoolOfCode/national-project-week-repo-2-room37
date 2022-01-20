efceb01f3c17e847fc76ad8255e544d52571d632
Mirella <mirellacodes@gmail.com>
2022-01-20 17:26:13 +0000

    added connstring

--- db/index.js
+++ db/index.js
@@ -3,7 +3,8 @@ import pg from "pg";
 // import { databaseHost, databaseName, password, username } from "../config.js";
 
 const pool = new pg.Pool({
-  connectionString: process.env.PGSTRING,
+  connectionString:
+    "postgres://eqwtnurqrllkwl:2e14765ea824f5d2a1c527fb7703372012698adc8b5aecc55b97fa27219e6644@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/d6gpjci143n2le",
   ssl: { rejectUnauthorized: false },
 });
 
