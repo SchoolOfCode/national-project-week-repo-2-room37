5d8850033e8d9de88942499e9de69591fa5d45cf
Mirella <mirellacodes@gmail.com>
2022-01-20 17:29:55 +0000

    moved back to env

--- db/index.js
+++ db/index.js
@@ -3,8 +3,7 @@ import pg from "pg";
 // import { databaseHost, databaseName, password, username } from "../config.js";
 
 const pool = new pg.Pool({
-  connectionString:
-    "postgres://eqwtnurqrllkwl:2e14765ea824f5d2a1c527fb7703372012698adc8b5aecc55b97fa27219e6644@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/d6gpjci143n2le",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
