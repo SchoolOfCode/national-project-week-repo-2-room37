f9c19e4ff7d62855bfbee32bf0705db6d1b922c5
Mirella <mirellacodes@gmail.com>
2022-01-20 12:21:40 +0000

    modified index.js

--- db/index.js
+++ db/index.js
@@ -3,22 +3,7 @@ import pg from "pg";
 // import { databaseHost, databaseName, password, username } from "../config.js";
 
 const pool = new pg.Pool({
-<<<<<<< HEAD
-<<<<<<< HEAD
   connectionString: process.env.PGSTRING,
-
-=======
-=======
->>>>>>> parent of 9f5f3d6 (added connectionString)
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: 5432,
-<<<<<<< HEAD
->>>>>>> parent of 9f5f3d6 (added connectionString)
-=======
->>>>>>> parent of 9f5f3d6 (added connectionString)
   ssl: { rejectUnauthorized: false },
 });
 
